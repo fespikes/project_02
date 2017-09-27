2bd743d6b049044414cde22c1c726284b3c8a24d
rockGithub <rock20110414@gmail.com>
2017-09-27 23:51:08 +0800

    products changes

---
+++ src/app/components/products/common/components/scenarios/scenarios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tdc-scenarios',
+  templateUrl: './scenarios.component.html',
+  styleUrls: ['./scenarios.component.sass']
+})
+export class ScenariosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
