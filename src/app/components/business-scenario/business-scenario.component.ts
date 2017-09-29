af15f523b1634c845edb44b4090546ce0d6a85c5
rockGithub <rock20110414@gmail.com>
2017-09-29 14:38:47 +0800

    code changes

--- src/app/components/business-scenario/business-scenario.component.ts
+++ src/app/components/business-scenario/business-scenario.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'tdc-business-scenario',
   templateUrl: './business-scenario.component.html',
-  //styleUrls: ['./business-scenario.component.sass']
+  styleUrls: ['./business-scenario.component.sass']
 })
 export class BusinessScenarioComponent implements OnInit {
 
