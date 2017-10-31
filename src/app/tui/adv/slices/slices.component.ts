6a6a3eeba98d8118018b3787f343fd7e408b9e2d
rockGithub <rock20110414@gmail.com>
2017-10-31 16:08:16 +0800

    TODO

--- src/app/tui/adv/slices/slices.component.ts
+++ src/app/tui/adv/slices/slices.component.ts
@@ -42,7 +42,7 @@ export class SlicesComponent implements OnInit {
     this.windowWidth = 3 * styleConfig.itemWidth;
 
   	this.interval = setInterval(_=>{
-  		this.goRight();
+  		// this.goRight();
   	}, 5000);
 
   }
@@ -50,10 +50,11 @@ export class SlicesComponent implements OnInit {
   goLeft() { }
 
   goRight() {
-    let ml, sl;
+    let ml, sl, swapMl, swapSl;
     const nativeElement = this.el.nativeElement;
     const ulProducts = nativeElement.querySelector('.ul-products');
     const ulSwap = nativeElement.querySelector('.ul-swap');
+
 
     if (this.itemsLength>3) {
       ml = parseInt(ulProducts.style.left);   //main ul left
@@ -62,18 +63,29 @@ export class SlicesComponent implements OnInit {
 
       if (ml<=0 && ml>this.itemsWidth*-1) {
 
-        ulSwap.style.left = this.windowWidth + 'px';
-        ulProducts.style.left = ml - this.windowWidth + 'px';//TODO: animation of fade to left slowly
+        ulSwap.style.left = (swapSl = this.windowWidth) + 'px';
+        ulProducts.style.left = (swapMl = ml - this.windowWidth) + 'px';//TODO: animation of fade to left slowly
 
         if (ml==(this.itemsWidth-this.windowWidth)*-1) {
           ulSwap.style.left = 0 + 'px';
         }
+
+        //for not divisible items
+        if( 0<swapMl+this.itemsWidth && swapMl+this.itemsWidth<this.windowWidth ){
+          ulSwap.style.left = this.itemsWidth + swapMl;
+        }
       } else {
-        ulProducts.style.left = this.windowWidth + 'px';
-        ulSwap.style.left = sl - this.windowWidth + 'px';
-        if (sl = (this.itemsWidth - this.windowWidth) * -1) {
+        ulProducts.style.left = (swapMl = this.windowWidth) + 'px';
+        ulSwap.style.left = (swapSl = sl - this.windowWidth) + 'px';
+
+        if (sl = this.itemsWidth*-1) {
           ulProducts.style.left = '0px';
         }
+
+        if( 0<swapSl+this.itemsWidth && swapSl+this.itemsWidth<this.windowWidth ){
+          ulProducts.style.left = this.itemsWidth + swapSl;
+        }
+
       }
     }
   }
