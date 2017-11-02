5ef8a344a3671e1215aa8a012d909b3a06f6fe7a
rockGithub <rock20110414@gmail.com>
2017-11-02 17:35:53 +0800

    products: tabs: fixed position when scroll to top

--- src/app/components/products/common/components/tabs/tabs/tabs.component.ts
+++ src/app/components/products/common/components/tabs/tabs/tabs.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'tabs',
   templateUrl: './tabs.component.html',
   styleUrls: ['./tabs.component.sass']
 })
-export class TabsComponent implements OnInit {
+export class TabsComponent implements OnInit, OnDestroy {
 
-	public tabs: any;
+  public tabs: any;
 
-  constructor() { }
+  private tabUlClassName = 'tabsUl';
+
+  constructor(
+  	private el:ElementRef
+	) { }
 
   ngOnInit() {
+  	document.onscroll = this.manipulateTab.bind(this);
+  }
+
+  manipulateTab(e) {
+  	const scrollTop = 402;  	//XXXX: get the scrollTop flexibly;
+  	const tabsUl = this.el.nativeElement.querySelector('.tabsUl');
+
+  	if (document.documentElement.scrollTop<scrollTop) {
+  		tabsUl.className = this.tabUlClassName;
+  		return ;
+
+  	} else {
+  		tabsUl.className = 'fixed ' + this.tabUlClassName;
+  	}
+  }
+
+  ngOnDestroy() {
+  	document.onscroll = null;
   }
 
 }
