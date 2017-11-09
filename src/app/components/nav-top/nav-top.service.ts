9939f6155008c589af5df1584c0c6a9dae6c416c
rockGithub <rock20110414@gmail.com>
2017-11-09 11:27:41 +0800

    top menu: adding products router link

--- src/app/components/nav-top/nav-top.service.ts
+++ src/app/components/nav-top/nav-top.service.ts
@@ -59,34 +59,34 @@ const navigationData = {
 				{
 				  title: '数据仓库',
 				  summary: '用于构建一站式数据仓库服务，提供包括分析数据库、数据整合、模型加工等重要功能，同时提供对于查询速度、并发度、扩展性的保障，是高效的数据掘金利器。',
-				  routerLink: ''
+				  routerLink: './products/data-warehouse'
 				},{
 				  title: '数据集市',
 				  summary: '用于为企业构建数据集市，面向特定的应用主题，对接数据分析业务，提供包括Cube设计、交互式分析、可视化分析、调度在内的多种工具，实现自动化的报表应用构建。',
-				  routerLink: ''
+				  routerLink: './products/data-mart'
 				},
 				{
 				  title: '信息检索',
 				  summary: '为企业构建一站式内部搜索平台和信息检索服务，提供PB级别的高速全文检索，实现毫秒级的响应速度，高并发支持，字段精确和模糊检索以及快速统计功能。',
-				  routerLink: ''
+				  routerLink: './products/info-retrieval'
 				},
 				{
 				  title: '关系数据库',
 				  summary: '构建企业内部的关系数据库服务，可用于处理数据量在500GB以内的OLTP业务。支持复杂的SQL查询，提供高稳定、可扩展、强一致的数据处理服务。',
-				  routerLink: ''
+				  routerLink: './products/relational-database'
 				},
 				{
 				  title: '深度学习',
 				  summary: '用于深度学习和人工智能开发的数据处理平台，帮助企业开发深度学习和AI应用，实现高度智能化的信息处理，协助与增进企业运作管理。',
-				  routerLink: ''
+				  routerLink: './products/deep-learning'
 				},{
 				  title: '实时计算',
 				  summary: '云上的流处理分析平台，可对流数据进行实时采集和实时处理。为企业构建实时数据仓库，开发多种在线实时应用，实现对流式信息的充分利用。',
-				  routerLink: ''
+				  routerLink: './products/realtime-computation'
 				},{
 				  title: '数据分析与挖掘',
 				  summary: '提供数据清洗、特征工程、建模平台和工具集，以及60余种分布式机器学习算法和多种行业模型，方便探索对象属性和行为之间的关系，从数据中发现蕴含价值。',
-				  routerLink: ''
+				  routerLink: './products/analysis-mining'
 				}
 			]
 		},
