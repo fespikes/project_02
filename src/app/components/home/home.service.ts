45e59260f669128011c65a6699fa2ad38b567ac0
rockGithub <rock20110414@gmail.com>
2017-11-14 13:27:22 +0800

    home: cloud products adding routerlink

--- src/app/components/home/home.service.ts
+++ src/app/components/home/home.service.ts
@@ -47,7 +47,7 @@ export class HomeService {
 					summary: '构建企业内部的关系数据库服务，可用于处理数据量在500GB以内的OLTP业务。支持复杂的SQL查询，提供高稳定、可扩展、强一致的数据处理服务。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/relational-database'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -65,7 +65,7 @@ export class HomeService {
 					summary: '数据集市是面向部门级业务，特定的应用主题，用星型或者雪花模型组织构建数据。TDH-DM可以用于构建企业的数据集市，对接数据分析业务，提供包括交互式分析、Cube、报表、调度工具，实现自动化的报表应用构建。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/data-mart'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -83,7 +83,7 @@ export class HomeService {
 					summary: '提供PB级别的高速全文检索服务，实现毫秒级的响应速度，支持用SQL做内容检索，提供多种接口，高并发支持，字段精确和模糊检索以及快速统计功能。采用异地数据共享分布式存储，存储海量数据，为企业构建一站式内部搜索平台和信息检索服务。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/info-retrieval'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -101,7 +101,7 @@ export class HomeService {
 					summary: '数据仓库是数据中心库，集中整合最新以及历史数据，按照不同主题组织信息，是打造商业智能的核心。TDH-DW可用于构建一站式数据仓库服务，提供包括分析数据库、数据整合、模型加工等重要功能，同时提供对于查询速度、并发度、扩展性的保障，是高效的数据掘金利器。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/data-warehouse'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -120,7 +120,7 @@ export class HomeService {
 					summary: '深度学习产品TDH-DL是用于深度学习和人工智能开发的数据处理平台，帮助企业开发深度学习和AI应用，实现高度智能化的信息处理，将某些投入简单重复劳动中的人力解放出来，协助与增进企业运作管理。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/deep-learning'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -140,7 +140,7 @@ export class HomeService {
 					summary: '云上的流处理分析平台，可对流数据进行实时采集和实时处理。帮助企业构建实时数据仓库，开发如传感器数据分析，异常状态检测，风险分析，在线反欺诈等多种在线实时应用，实现对流式信息的充分利用。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/realtime-computation'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
@@ -160,7 +160,7 @@ export class HomeService {
 					summary: '数据分析平台为数据科学家提供了数据清洗、特征工程、建模平台以及工具集，用于构建数据分析、数据挖掘业务。共提供了60余种分布式机器学习算法和多种行业模型，方便从数据中发现蕴含价值，探索对象属性和行为之间的关系，将数据有效转换为助于企业运营的价值信息。',
 					button: {
 						text: '查看详情',
-						routerLink: ''//TODO: set the routerLinks
+						routerLink: '/products/analysis-mining'//TODO: set the routerLinks
 					},
 					details: {
 						title: '产品功能',
