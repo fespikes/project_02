d879a63392624322c473f4d0901590b5be384ff4
haitao <haitao.lv@transwarp.io>
2017-11-15 21:27:48 +0800

    modules: add other product module link

--- src/app/components/products/info-retrieval/info-retrieval.service.ts
+++ src/app/components/products/info-retrieval/info-retrieval.service.ts
@@ -42,7 +42,7 @@ export class InfoRetrievalService {
         //TODO: image style abstraction
         content: [
           {
-            title: '全文搜索', 
+            title: '全文搜索',
             text: '支持全文检索，提供精确和模糊的字段查询功能。可跨越数据源、数据格式实现检索，结果返回文件路径和关键词出现的片段，高亮显示查询内容，检索结果按照关联度排序。'
           },
           {
@@ -114,19 +114,19 @@ export class InfoRetrievalService {
             ctn: [{
               title: 'Inceptor',
               text: 'Inceptor是一款用于批量处理及分析的数据库。它支持SQL 2003标准、Oracle PL/SQL以及DB2 SQL PL，对Oracle、DB2以及Teradata 都有很好的方言支持，是Hadoop领域对SQL标准支持最完善的产品。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/product/inceptor'
             },{
               title: 'Guardian',
               text: 'Guardian为TDH提供集中的安全和资源管理服务。它支持LDAP和Kerberos，保护Hadoop集群免受恶意攻击和安全威胁，而且还可以对资源做细粒度的ACL控制。多租户资源管理模块可以按照租户的方式管理资源，并通过一个图形化工具为用户提供权限配置以及资源配置接口。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/product/guardian'
             }, {
               title: 'Search',
               text: 'Search用于在企业内部构建大数据搜索引擎。它能够在PB数据量级上实现秒级延迟的搜索功能;在开发接口方面，Search提供了完整 的SQL语法支持并提供了搜索语法SQL扩展，通过和Inceptor优化器有效结合，使开发者无需了解底层架构就可以开发出高效的搜索引 擎。Search创新的使用了堆外内存管理技术来提高系统的健壮性，避免了GC问题对系统的影响;此外，Search还支持混合存储，通过 将热数据存储在SSD上来提升查询速度。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/product/search'
             },{
               title: 'Hadoop',
               text: 'Hadoop Distributed File System (HDFS) — Apache Hadoop 项目的一个子项目 — 是一个高度容错的分布式文件系统，设计用于在低成本硬件上运行。HDFS 提供高吞吐量应用程序数据访问功能，适合带有大型数据集的应用程序。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/product/hadoop'
             }]
           },
           {
@@ -134,7 +134,7 @@ export class InfoRetrievalService {
             ctn: [{
               title: 'Pilot',
               text: 'Pilot是基于Web的报表展现工具，轻量、灵活，可以快速部署。它支持多维度的分析和自助分析，提供数十种报表样式，对时序数据 也有很好的展现。此外，Pilot还支持团队协作和共享，支持导入和导出报表。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/suite/pilot'
             }],
           },
           {
@@ -142,20 +142,20 @@ export class InfoRetrievalService {
             ctn: [{
               title: 'Milano',
               text: 'Milano是一个日志收集分析和展示平台，为运维人员提供基于日志问题的定位和分析工具。它具有五个功能组件：logstash、kafka、elasticsearch、kibana、flink，分别用来帮助用户实现日志收集、传输、存储、展示和分析这五项功能。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/tool/milano'
             },{
               title: 'Terminal',
               text: 'Terminal 容器命令行是集群上服务用命令行指令操作的入口，它集成了各个服务的客户端，可以在网页上连接并操作集群内的各个服务，避免了需要登录各个容器才可以操作服务的烦恼。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/tool/terminal'
             },{
               title: 'Prometheus',
               text: 'Prometheus是开源的监控和告警系统，以Metrics监控为主，通过pull mode完成时间序列数据的收集。主要包括的组件有：负责时间序列数据存储和抓取的Prometheus Server，Alertmanager，Push gateway，以及各类exporter。',
-              link: ''  //TODO: the link goto incepter introduction
+              link: '../../modules/tool/prometheus'
             }]
           }
         ]
       },
-      
+
   	}
   }
 }
