15052a4d25d01978a0f54f1195afe4d3582d45f4
rockGithub <rock20110414@gmail.com>
2017-11-18 00:30:24 +0800

    router effect: scroll to top by default

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,18 +31,16 @@ export class AppComponent {
 
   	this.router.events.subscribe(e=>{
   		if(e instanceof NavigationStart){
-  			console.log('NavigationStart');
+  			scrollTo(0, 0);
   		}
 
   		if(e instanceof RoutesRecognized){
-  			console.log('RoutesRecognized');
   		}
 
   		if(e instanceof NavigationEnd){
-  			console.log('NavigationEnd:');
-
-  			this.routerState = !this.routerState;
-  			this.routerStateCode = this.routerState ? 'active' : 'inactive';
+        //TODO: 
+  			// this.routerState = !this.routerState;
+  			// this.routerStateCode = this.routerState ? 'active' : 'inactive';
   		}
 
   	});
