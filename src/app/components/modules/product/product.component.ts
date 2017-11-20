7965c5d8bbd05d06769c2d15a5d20dde41d38b1b
haitao <haitao.lv@transwarp.io>
2017-11-20 18:06:42 +0800

    modules: change milano introduce picture

--- src/app/components/modules/product/product.component.ts
+++ src/app/components/modules/product/product.component.ts
@@ -42,23 +42,26 @@ export class ProductComponent implements OnInit {
   }
 
   anchorContent(tab) {
-    const anchorTag = tab.tabName === this.MODULE_ADVANTAGE_TAG
-      ? this.MODULE_ADVANTAGE_TAG : this.MODULE_INTRODUCE_TAG;
+    const anchorTag = tab.tabName === 'advantage' ? this.MODULE_ADVANTAGE_TAG
+      : this.MODULE_INTRODUCE_TAG;
     const scrollTop = this.getScrollTop(tab.tabName, tab.className);
     this.productUtilService.anchorTabContent(anchorTag, scrollTop);
   }
 
   getScrollTop(tabName, className) {
-    let scrollTop = 0;
+    let scrollTop = this.ANCHOR_TOP;
     if(className === 'tab-ul') {
       if (tabName === 'introduce') {
         scrollTop = this.ANCHOR_TOP - this.TAB_HEIGHT;
       } else if (tabName === 'advantage') {
         scrollTop = this.ANCHOR_TOP - 2 * this.TAB_HEIGHT;
       }
-    }
-    if(tabName === 'introduce' && className === 'fixed') {
-      scrollTop = this.ANCHOR_TOP;
+    }else if(className === 'fixed') {
+      if(tabName === 'introduce') {
+        scrollTop = this.ANCHOR_TOP;
+      }else if(tabName === 'advantage') {
+        scrollTop = this.ANCHOR_TOP - this.TAB_HEIGHT;
+      }
     }
     return scrollTop;
   }
