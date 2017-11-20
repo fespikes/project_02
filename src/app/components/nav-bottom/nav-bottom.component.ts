7344885b55b1d362627f114089188219a9bf9403
rockGithub <rock20110414@gmail.com>
2017-11-20 13:12:11 +0800

    nav bottom: adding the link to customer service on bottom nav

--- src/app/components/nav-bottom/nav-bottom.component.ts
+++ src/app/components/nav-bottom/nav-bottom.component.ts
@@ -1,17 +1,28 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, AfterViewChecked } from '@angular/core';
 
 @Component({
   selector: 'tdc-nav-bottom',
   templateUrl: './nav-bottom.component.html',
   styleUrls: ['./nav-bottom.component.sass']
 })
-export class NavBottomComponent implements OnInit {
+export class NavBottomComponent implements OnInit, AfterViewChecked {
 
   @HostBinding('class.nav-bottom-box') nav = true;
 
   constructor() { }
 
+  private csButton: any; 
+
   ngOnInit() {
+  }
+
+  toCustomerService($event) {
+  	this.csButton && this.csButton.click();
+  }
+
+  ngAfterViewChecked() {
+  	let csButtonId = '#__ewei__box__webchat__button__';
+  	this.csButton = document.querySelector(csButtonId);
   }
 
 }
