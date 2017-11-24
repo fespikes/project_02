4ba57d7a1e7edbd693e53c555856119dd9254263
rockGithub <rock20110414@gmail.com>
2017-11-24 11:17:49 +0800

    merge from feature/FE to dev

--- src/app/components/home/home.service.ts
+++ src/app/components/home/home.service.ts
@@ -606,9 +606,10 @@ export class HomeService {
 					{
 						thumbnail: {
 							src: '/assets/images/home/tutorial-videos/Inceptor1.jpg',
+							href: 'http://v.qq.com/x/page/t0507jc08pw.html'
 						},
 						name: 'Inceptor',
-						title: '分析型数据库',
+						title: '1.分析型数据库',
 						summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
 						h5Class: '',	//?
 						pClass: 'desc',	//?
@@ -617,9 +618,10 @@ export class HomeService {
 					{
 						thumbnail: {
 							src: '/assets/images/home/tutorial-videos/workflow.jpg',
+							href: 'http://v.qq.com/x/page/a0507bao7c8.html'
 						},
 						name: 'Inceptor',
-						title: 'workflowTitle',
+						title: '2.workflowTitle',
 						summary: 'workflow是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
 						h5Class: '',	//?
 						pClass: 'desc',	//?
@@ -628,9 +630,10 @@ export class HomeService {
 					{
 						thumbnail: {
 							src: '/assets/images/home/tutorial-videos/Inceptor2.jpg',
+							href: 'http://v.qq.com/x/page/e0507nmt9qg.html'
 						},
 						name: 'Inceptor',
-						title: '分析型数据库',
+						title: '3.分析型数据库',
 						summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
 						h5Class: '',	//?
 						pClass: 'desc',	//?
@@ -639,9 +642,46 @@ export class HomeService {
 					{
 						thumbnail: {
 							src: '/assets/images/home/tutorial-videos/workflow.jpg',
+							href: 'http://v.qq.com/x/page/y0507qw8cg9.html'
 						},
 						name: 'Inceptor',
-						title: '分析型数据库',
+						title: '4.分析型数据库',
+						summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+						h5Class: '',	//?
+						pClass: 'desc',	//?
+						divClass: 'shadow-box'	//has a div for wrap
+					},
+					{
+						thumbnail: {
+							src: '/assets/images/home/tutorial-videos/workflow.jpg',
+							href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
+						},
+						name: 'Inceptor',
+						title: '5.workflowTitle',
+						summary: 'workflow是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+						h5Class: '',	//?
+						pClass: 'desc',	//?
+						divClass: 'shadow-box'	//has a div for wrap
+					},
+					{
+						thumbnail: {
+							src: '/assets/images/home/tutorial-videos/Inceptor2.jpg',
+							href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
+						},
+						name: 'Inceptor',
+						title: '6.分析型数据库',
+						summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+						h5Class: '',	//?
+						pClass: 'desc',	//?
+						divClass: 'shadow-box'	//has a div for wrap
+					},
+					{
+						thumbnail: {
+							src: '/assets/images/home/tutorial-videos/workflow.jpg',
+							href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
+						},
+						name: 'Inceptor',
+						title: '7.分析型数据库',
 						summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
 						h5Class: '',	//?
 						pClass: 'desc',	//?
