4de4593722e388c6831a88f00f3848712c26fc59
rockGithub <rock20110414@gmail.com>
2017-12-08 20:00:29 +0800

    home: video tutorial： Screenshot replace

--- src/app/components/common/services/common.service.ts
+++ src/app/components/common/services/common.service.ts
@@ -21,91 +21,91 @@ export class CommonService {
 			items: [
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/Inceptor1.jpg',
-						href: 'http://v.qq.com/x/page/t0507jc08pw.html'
+						src: '/assets/images/svg/home/video-tutorial/admin.svg',
+						href: 'http://v.qq.com/x/page/t0507jc08pw.html'//TODO:
 					},
-					routerLink: '/video-tutorial/inceptor01',
+					routerLink: '/video-tutorial/inceptor01',//TODO
 					name: 'Inceptor',
-					title: '1.分析型数据库',
-					summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和数据集市的构建。',
+					title: '管理中心教程',
+					summary: '管理中心四大模块操作演示，快速了解功能与实现方式',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/workflow.jpg',
+						src: '/assets/images/svg/home/video-tutorial/products.svg',
 						href: 'http://v.qq.com/x/page/a0507bao7c8.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '2.workflowTitle',
-					summary: 'workflow是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: '云产品开发演示',
+					summary: '如何快速构建云产品，进行功能组合，实现数据开发',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/Inceptor2.jpg',
+						src: '/assets/images/svg/home/video-tutorial/pilot.svg',
 						href: 'http://v.qq.com/x/page/e0507nmt9qg.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '3.分析型数据库',
-					summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: '报表设计Pilot',
+					summary: '利用Pilot以自助的方式实现华丽炫酷的报表定制',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/workflow.jpg',
+						src: '/assets/images/svg/home/video-tutorial/workflow.svg',
 						href: 'http://v.qq.com/x/page/y0507qw8cg9.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '4.分析型数据库',
-					summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: '工作流调度Workflow',
+					summary: '使用Workflow在集群内实现自动化的工作流调度',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/workflow.jpg',
+						src: '/assets/images/svg/home/video-tutorial/transporter.svg',
 						href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '5.workflowTitle',
-					summary: 'workflow是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: 'ETL工具Transporter',
+					summary: '通过Transporter实现多种数据源之间的数据处理与传导',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/Inceptor2.jpg',
+						src: '/assets/images/svg/home/video-tutorial/governor.svg',
 						href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '6.分析型数据库',
-					summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: '数据治理Governor',
+					summary: '利用元数据监控管理平台Governor把控数据质量',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
 				},
 				{
 					thumbnail: {
-						src: '/assets/images/home/tutorial-videos/workflow.jpg',
+						src: '/assets/images/svg/home/video-tutorial/rubik.svg',
 						href: 'http://v.qq.com/x/page/s0507hqw4vt.html'
 					},
 					routerLink: '/video-tutorial/inceptor02',
 					name: 'Inceptor',
-					title: '7.分析型数据库',
-					summary: 'Inceptor是一款用于批量处理及分析的数据库，Inceptor被广泛地应用于数据仓库和 数据集市的构建。',
+					title: 'OLAP Cube设计Rubik',
+					summary: '通过Rubik实现数据立方体设计加速多维交互式分析',
 					h5Class: 'video-title',	//?
 					pClass: 'description',	//?
 					divClass: 'shadow-box'	//has a div for wrap
